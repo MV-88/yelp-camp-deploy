16f1dc0d546ad321a15bfcd26846fa7d9dd9efbd
Your Name <you@example.com>
2020-10-08 23:39:46 +0000

    add env.PORT

--- app.js
+++ app.js
@@ -56,8 +56,16 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen("3000", function(){
-	console.log("The YelpCamp Server Has Started!")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${ PORT }`);
 });
+
+
+// app.listen("3000", function(){
+// 	console.log("The YelpCamp Server Has Started!")
+// });
+
+
 
 
